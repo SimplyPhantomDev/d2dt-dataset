e8d3d030d398e7a41ca963ef8a284d6f30df6f47
Tomi Niemelä <tomi.s.niemela@tuni.fi>
2025-12-16 01:50:42 +0200

    Modify fetchMatchups to work on workflow

--- generator/fetchMatchups.js
+++ generator/fetchMatchups.js
@@ -1,5 +1,8 @@
 const path = require("path");
-require("dotenv").config({ path: path.resolve(__dirname, "../.env") });
+require("dotenv").config({
+  path: path.resolve(__dirname, "../.env"),
+  override: false
+});
 
 if (!process.env.STRATZ_API_TOKEN) {
   throw new Error(
